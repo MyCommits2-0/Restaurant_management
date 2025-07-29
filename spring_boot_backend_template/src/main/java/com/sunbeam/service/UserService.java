ccbfb09397e940e7863c978b4fca285b14f95827
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2025-07-29 15:35:29 +0530

    service layer created named 1st UserService

---
+++ spring_boot_backend_template/src/main/java/com/sunbeam/service/UserService.java
@@ -0,0 +1,16 @@
+package com.sunbeam.service;
+
+import com.sunbeam.dto.AddressDTO;
+import com.sunbeam.dto.ApiResponse;
+import com.sunbeam.dto.AuthRequest;
+import com.sunbeam.dto.UserRequestDTO;
+import com.sunbeam.dto.UserRespDTO;
+
+public interface UserService {
+//sign in
+	UserRespDTO authenticate(AuthRequest dto);
+
+	UserRespDTO signUp(UserRequestDTO dto);
+
+	ApiResponse assignAddress(Long userId, AddressDTO dto);
+}
