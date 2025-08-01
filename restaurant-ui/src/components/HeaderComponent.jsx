ef1f79a9a7f125ad6c1f6798594b356ffe71d228
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2025-08-01 09:07:20 +0530

    Header component created

---
+++ restaurant-ui/src/components/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+// src/components/HeaderComponent.jsx
+import { Link } from 'react-router-dom';
+
+const HeaderComponent = () => {
+  return (
+    <header className="bg-indigo-600 text-white py-4 px-8">
+      <h1 className="text-2xl font-bold">Restaurant Management System</h1>
+    </header>
+  );
+};
+
+export default HeaderComponent;
